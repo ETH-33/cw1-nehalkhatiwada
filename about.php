5214e414838b8ccccd40e626b719042439a66293
nehalkhatiwada <nehalkhatiwada2542@gmail.com>
2023-07-12 20:28:38 +0545

    readd

---
+++ about.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php include("vendor/inc/head.php");?>
+
+<body>
+
+  
+  <?php include("vendor/inc/nav.php");?>
+
+  
+  <div class="container">
+
+    
+    <h1 class="mt-4 mb-3">About
+    </h1>
+
+    <ol class="breadcrumb">
+      <li class="breadcrumb-item">
+        <a href="index.php">Home</a>
+      </li>
+      <li class="breadcrumb-item active">About</li>
+    </ol>
+
+    
+    <div class="row">
+      <div class="col-lg-6">
+        <img class="img-fluid rounded mb-5" src="vendor/img/placeholder-1.jpg" alt="">
+      </div>
+      <div class="col-lg-6">
+        <h2>About Us</h2>
+        <p>
+        The Vehicle Booking System was developed as a solution to common transportation issues. Task allocation, vehicle tracking, route assignment, payment, booking order, delivery report, and report generation are only some of the transport industry's biggest challenges.The time and money needed for processing transactions, staff overwork, product and user safety, and vehicle upkeep and repair.Major issues that can be addressed by the Vehicle Booking System include task allocation (a GPRS will be installed in all vehicles and cabs, allowing for easy tracking of each vehicle and cab) and financial reporting (the system will ensure that all payments are processed via credit card or pay bill, eliminating the need for manual record-keeping). The system will be able to see which cars have duties to do and which do not, as well as data about each client who has reserved a vehicle.
+
+        </p>
+      </div>
+    </div>
+    <!-- /.row -->
+  </div>
+  <!-- /.container -->
+
+  <!-- Footer -->
+<?php include("vendor/inc/footer.php");?>
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+</body>
+
+</html>
