f9a1acbe43c8326221a924546320ec10eb2e8956
nehalkhatiwada <nehalkhatiwada2542@gmail.com>
2023-07-13 22:06:51 +0545

    home

---
+++ home.php
@@ -0,0 +1,162 @@
+<?php
+
+include 'components/connect.php';
+
+session_start();
+
+if(isset($_SESSION['user_id'])){
+   $user_id = $_SESSION['user_id'];
+}else{
+   $user_id = '';
+};
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Welcome</title>
+
+   <link rel="stylesheet" href="https://unpkg.com/swiper@8/swiper-bundle.min.css" />
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+   <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+<section class="hero">
+
+   <div class="swiper hero-slider">
+
+      <div class="swiper-wrapper">
+
+         <div class="swiper-slide slide">
+            <div class="content">
+               <span>Contact US</span>
+               <h3>Tasty Foods Available</h3>
+               <a href="menu.php" class="btn">order</a>
+            </div>
+            <div class="image">
+               <img src="images/home-1.jpg" alt="">
+            </div>
+         </div>
+
+         <div class="swiper-slide slide">
+            <div class="content">
+               <span>Contact Us</span>
+               <h3>Nepali Khana Set</h3>
+               <a href="menu.php" class="btn">See Menu</a>
+            </div>
+            <div class="image">
+               <img src="images/homekhana.jpg" alt="">
+            </div>
+         </div>
+
+         <div class="swiper-slide slide">
+            <div class="content">
+               <span>Contact Us </span>
+               <h3>Special Pani-puri</h3>
+               <a href="menu.php" class="btn">See Menu</a>
+            </div>
+            <div class="image">
+               <img src="images/home-3.jpg" alt="">
+            </div>
+         </div>
+
+      </div>
+
+      <div class="swiper-pagination"></div>
+
+   </div>
+
+</section>
+
+<section class="category">
+
+   <h1 class="title">Food category</h1>
+
+   <div class="box-container">
+
+      <a href="category.php?category=fast food" class="box">
+         <img src="images/cat-1.png" alt="">
+         <h3>Fast food</h3>
+      </a>
+
+      <a href="category.php?category=main dish" class="box">
+         <img src="images/cat-2.png" alt="">
+         <h3>Main Course</h3>
+      </a>
+
+      <a href="category.php?category=drinks" class="box">
+         <img src="images/cat-3.png" alt="">
+         <h3>Drinks</h3>
+      </a>
+
+      
+
+   </div>
+
+</section>
+
+
+
+
+<section class="products">
+
+   <h1 class="title">latest Meals</h1>
+
+   <div class="box-container">
+
+      
+      <form action="" method="post" class="box">
+         <input type="hidden" name="pid" value="<?= $fetch_products['id']; ?>">
+         <input type="hidden" name="name" value="<?= $fetch_products['name']; ?>">
+         <input type="hidden" name="price" value="<?= $fetch_products['price']; ?>">
+         <input type="hidden" name="image" value="<?= $fetch_products['image']; ?>">
+         <a href="quick_view.php?pid=<?= $fetch_products['id']; ?>" class="fas fa-eye"></a>
+         <button type="submit" class="fas fa-shopping-cart" name="add_to_cart"></button>
+         <img src="uploaded_img/<?= $fetch_products['image']; ?>" alt="">
+         <a href="category.php?category=<?= $fetch_products['category']; ?>" class="cat"><?= $fetch_products['category']; ?></a>
+         <div class="name"><?= $fetch_products['name']; ?></div>
+         <div class="flex">
+            <div class="price"><span>RS</span><?= $fetch_products['price']; ?></div>
+            <input type="number" name="qty" class="qty" min="1" max="99" value="1" maxlength="2">
+         </div>
+      </form>
+     
+
+   </div>
+
+   <div class="more-btn">
+      <a href="menu.php" class="btn">veiw all</a>
+   </div>
+
+</section>
+<?php include 'components/footer.php'; ?>
+
+
+<script src="https://unpkg.com/swiper@8/swiper-bundle.min.js"></script>
+
+
+<script src="js/script.js"></script>
+
+<script>
+
+var swiper = new Swiper(".hero-slider", {
+   loop:true,
+   grabCursor: true,
+   effect: "flip",
+   pagination: {
+      el: ".swiper-pagination",
+      clickable:true,
+   },
+});
+
+</script>
+
+</body>
+</html>
